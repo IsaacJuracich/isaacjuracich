9085a544359612aba47ba0684b9a2cad779c6874
IsaacJuracich <93289395+IsaacJuracich@users.noreply.github.com>
2022-08-16 08:49:18 -0700

    Update hireme.js

--- pages/hireme.js
+++ pages/hireme.js
@@ -103,13 +103,13 @@ export default function HireMe() {
         </div>
         <div className="text-1xl text-center">
           <div className={styles.code}>
-            <a href="mailto=isaac.juracich@icloud.com">
+            <a href="mailto:isaac.juracich@icloud.com">
               isaac.juracich@icloud.com
             </a>
           </div>
           <br />
           <div className={styles.code}>
-            <a href="tel=3604104482">3604104482</a>
+            <a href="tel:3604104482">3604104482</a>
           </div>
         </div>
 
