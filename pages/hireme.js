d03023aa9999fcde27c909a48a5a0fa590a7637c
IsaacJuracich <93289395+IsaacJuracich@users.noreply.github.com>
2022-08-16 08:37:33 -0700

    s

--- pages/hireme.js
+++ pages/hireme.js
@@ -3,10 +3,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const { isBlurred, setBlurred } = useMyContext();
-  const router = useRouter();
-
+export default function HireMe() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -17,7 +14,7 @@ export default function Home() {
           data-aos-delay={300}
           data-aos-once="true"
         >
-          Isaac
+          Hire
         </h1>
         <h1
           className="font-extrabold lg:text-9xl md:text-8xl text-transparent text-7xl bg-clip-text bg-gradient-to-r from-indigo-500 to-purple-500 blur-sm"
@@ -26,7 +23,7 @@ export default function Home() {
           data-aos-delay={300}
           data-aos-once="true"
         >
-          Juracich
+          Me
         </h1>
         <div
           className="blur-sm"
@@ -40,7 +37,6 @@ export default function Home() {
             width: "75vh",
           }}
         />
-
         <div
           className="mt-5 text-2xl flex justify-center items-center"
           style={{
@@ -54,11 +50,67 @@ export default function Home() {
             data-aos-delay={300}
             data-aos-once="true"
           >
-            I'm a Fullstack Developer who enjoys learning new technologies. My
-            favorite technologies right now are: Next.js, GraphQL, Node.js, and
-            MongoDB
+            Why Should you hire me?
           </h3>
         </div>
+        <div
+          className="mt-2 flex justify-center items-center"
+          style={{
+            maxWidth: "75vh",
+          }}
+        >
+          <h3
+            className="text-center transform transition duration-500 hover:scale-125"
+            data-aos="fade-right"
+            data-aos-duration={900}
+            data-aos-delay={300}
+            data-aos-once="true"
+          >
+            I'm a Fullstack Developer who's very proficient and passionate about
+            anything I build. Always straight forward with all of my clients. I
+            have a fast turn around time, and reasonably priced rates. There is
+            companies that are taking advantage of people by overpricing them,
+            and lying to them about their services.
+          </h3>
+        </div>
+        <div
+          className="blur-sm"
+          style={{
+            display: "block",
+            height: "1px",
+            border: "0px",
+            borderTop: "1px solid #ccc",
+            margin: "1em 0px",
+            padding: "0px",
+            width: "75vh",
+          }}
+        />
+        <div
+          className="text-2xl flex justify-center items-center pt-4 pb-2"
+          style={{
+            maxWidth: "75vh",
+          }}
+        >
+          <h3
+            className="font-semibold text-center transform transition duration-500 hover:scale-125"
+            data-aos="fade-right"
+            data-aos-duration={900}
+            data-aos-delay={300}
+            data-aos-once="true"
+          >
+            Contact Me
+          </h3>
+        </div>
+        <div className="text-1xl text-center">
+          <div className={styles.code}>
+            <a mailto="isaac.juracich@icloud.com">isaac.juracich@icloud.com</a>
+          </div>
+          <br />
+          <div className={styles.code}>
+            <a tel="3604104482">3604104482</a>
+          </div>
+        </div>
+
         <div className="pt-8">
           <button
             style={{
@@ -73,11 +125,11 @@ export default function Home() {
             data-aos-delay={300}
             data-aos-once="true"
             onClick={() => {
-              router.push("/projects");
+              router.push("/");
             }}
           >
             <h2 className="text-2xl transform transition duration-500 hover:scale-125 font-semibold">
-              My Projects
+              Go Back
             </h2>
           </button>
         </div>
@@ -95,11 +147,11 @@ export default function Home() {
             data-aos-delay={300}
             data-aos-once="true"
             onClick={() => {
-              router.push("/hireme");
+              router.push("/projects");
             }}
           >
             <h2 className="text-2xl transform transition duration-500 hover:scale-125 font-semibold">
-              Hire Me
+              My Projects
             </h2>
           </button>
         </div>
--- pages/projects.js
+++ pages/projects.js
@@ -37,7 +37,7 @@ export default function Projects() {
             width: "75vh",
           }}
         />
-        <div className="">
+        <div>
           <link
             href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
             rel="stylesheet"
@@ -82,10 +82,10 @@ export default function Projects() {
                             className="w-4 h-4 ml-2"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           >
                             <path d="M5 12h14"></path>
                             <path d="M12 5l7 7-7 7"></path>
@@ -95,10 +95,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -110,10 +110,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -161,10 +161,10 @@ export default function Projects() {
                             className="w-4 h-4 ml-2"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           >
                             <path d="M5 12h14"></path>
                             <path d="M12 5l7 7-7 7"></path>
@@ -174,10 +174,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -189,10 +189,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -241,10 +241,10 @@ export default function Projects() {
                             className="w-4 h-4 ml-2"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           >
                             <path d="M5 12h14"></path>
                             <path d="M12 5l7 7-7 7"></path>
@@ -254,10 +254,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
@@ -269,10 +269,10 @@ export default function Projects() {
                           <svg
                             className="w-4 h-4 mr-1"
                             stroke="currentColor"
-                            stroke-width="2"
+                            strokeWidth="2"
                             fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             viewBox="0 0 24 24"
                           >
                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
